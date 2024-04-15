a2c9a8d6c4ef7874d6c44c4c36d09fc555995a73
lhx <2122537936@qq.com>
2024-04-15 14:32:32 +0800

    测试提交

---
+++ pai-web/src/main/java/com/lhx/paiweb/PaiWebApplication.java
@@ -0,0 +1,17 @@
+package com.lhx.paiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaiWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaiWebApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  pai-web模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /2 '       \n" );
+    }
+
+}
